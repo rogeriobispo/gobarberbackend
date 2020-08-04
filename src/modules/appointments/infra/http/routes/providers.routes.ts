856e9c0844c6572f053d15d00568c61b2d08eff2
Rogério Bispo <rogerio_pd@yahoo.com.br>
2020-08-04 20:49:34 -0300

    controller availabilitys and routes

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import ProvidersController from '../controllers/ProvidersController'
+import ProviderMonthAvailabitlyController from '../controllers/ProviderMonthAvailabitlyController'
+import ProviderDayAvailabitlyController from '../controllers/ProviderDayAvailabitlyController'
 
 const ProvidersRouter = Router();
 
 ProvidersRouter.get('/', ProvidersController.index);
+ProvidersRouter.get('/:provider_id/month-availability', ProviderMonthAvailabitlyController.index);
+ProvidersRouter.get('/:provider_id/day-availability', ProviderDayAvailabitlyController.index);
 
 export default ProvidersRouter;
